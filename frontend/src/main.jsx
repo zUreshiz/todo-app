a80b6afc41c13d02f710758e6233a74e7f09243c
zUreshiz <hungdhk.work@gmail.com>
2025-09-15 17:05:43 +0700

    init fe

---
+++ frontend/src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+import { ToastContainer, toast } from "react-toastify";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "../src/pages/HomePage";
+import NotFound from "../src/pages/NotFound.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+    <App />
+  </StrictMode>
+);
